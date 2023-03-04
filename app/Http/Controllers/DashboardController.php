a3c0c76a96abc6ea30a5ae800b326d1c3545b1d3
chanakanaveen <chanakanaveen2016@gmail.com>
2023-03-04 18:32:29 +0530

    create religion chart

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,10 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function index(){
-        return view('dashboard');
+
+        // religion-chart start
+
+        // $result = DB::select('SELECT religion, COUNT(*) as count FROM people GROUP BY religion');
+        $result= DB::table('people')
+                ->select('religion', DB::raw('count(*) as Count'))
+                ->groupBy('religion')
+                ->get();
+
+        $data ="";
+        foreach($result as $val){
+            $data.="['".$val->religion."',".$val->Count."],";
+        }
+        $religionChart=$data;
+
+        // dd($religionChart);
+        return view('dashboard',compact('religionChart'));
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -20,7 +20,7 @@
                 <div class="col-lg-12">
                     <div class="card">
                         <div class="card-body">
-                            
+                            <div id="chart_div"></div>
                         </div>
                     </div>
                 </div>
@@ -29,4 +29,44 @@
         </div><!-- /.container-fluid -->
     </div>
     <!-- /.content -->
+@endsection
+
+@section('scripts')
+
+<!--Load the AJAX API-->
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script type="text/javascript">
+
+  // Load the Visualization API and the corechart package.
+  google.charts.load('current', {'packages':['corechart']});
+
+  // Set a callback to run when the Google Visualization API is loaded.
+  google.charts.setOnLoadCallback(drawChart);
+
+  // Callback that creates and populates a data table,
+  // instantiates the pie chart, passes in the data and
+  // draws it.
+  function drawChart() {
+
+    // Create the data table.
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Religion');
+    data.addColumn('number', 'count');
+    data.addRows([
+      <?php echo $religionChart; ?>
+
+    ]);
+
+    // Set chart options
+    var options = {'title':'Religion by Presentage',
+                   'width':700,
+                   'height':500,
+                   is3D: true,};
+
+    // Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+    chart.draw(data, options);
+  }
+</script>
+
 @endsection
